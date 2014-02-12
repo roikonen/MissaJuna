c76df711fd6522b38aad8a64917df722a6c2fd41
roikonen <roikonen@iki.fi>
2014-02-12 17:19:39 +0200

    Train data has some delay so with this commit I'm trying to get data more accurate with reality

--- app/fi/proweb/train/actor/component/decorator/TrainDecorator.scala
+++ app/fi/proweb/train/actor/component/decorator/TrainDecorator.scala
@@ -11,8 +11,13 @@ object TrainDecorator {
 
 class TrainDecorator(locLat: Double, locLon: Double) extends AppDataDecorator[Train] {
 
+  val SPEED_KM_H = 150
+  val SUBSTRACT_DISTANCE_IN_M = 500
+  
   override def decorate(train: Train): Train = {
-    train.distance = Some(DistanceCalculator.countDistance(train.location.get._1, train.location.get._2, locLat, locLon))
+    val substraction = (SUBSTRACT_DISTANCE_IN_M / SPEED_KM_H) * train.speed.get
+    
+    train.distance = Some(DistanceCalculator.countDistance(train.location.get._1, train.location.get._2, locLat, locLon)-substraction)
     train
   }
   
